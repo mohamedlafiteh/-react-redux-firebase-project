81b43d0377047961dc8b1a14a107c726e96ee623
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-12-10 17:11:15 +0000

    Inishlise firebase connection

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAq1klshvtgdjt33ecKL9gFPgKEczhC-Q",
+  authDomain: "react-redux-project-c710f.firebaseapp.com",
+  databaseURL: "https://react-redux-project-c710f.firebaseio.com",
+  projectId: "react-redux-project-c710f",
+  storageBucket: "react-redux-project-c710f.appspot.com",
+  messagingSenderId: "737259164278",
+  appId: "1:737259164278:web:a6d5dcca1daf8063fe4557",
+  measurementId: "G-CZNLT6QZQS"
+};
+
+firebase.intializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
